ad9fbdc2e29e17afa526ef8cab095f4de4dcc87f
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-05-27 20:49:33 +1000

    Update to build image with latest tag on develop branch
    And only build for develop branch for now

--- docker_build.sh
+++ docker_build.sh
@@ -5,10 +5,7 @@ set -o -e pipefail
 REGISTRY="registry.skycoin.net"
 
 # Build cx-tracker image
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ $TRAVIS_BRANCH == "develop" ]]; then
   docker build -t $REGISTRY/cx-tracker -f docker/images/cx-tracker/Dockerfile .
   docker build -t $REGISTRY/cx-tracker-web -f docker/images/cx-tracker-web/Dockerfile .
-elif [[ $TRAVIS_BRANCH == "develop" ]]; then
-  docker build -t $REGISTRY/cx-tracker:develop -f docker/images/cx-tracker/Dockerfile .
-  docker build -t $REGISTRY/cx-tracker-web:develop -f docker/images/cx-tracker-web/Dockerfile .
 fi
